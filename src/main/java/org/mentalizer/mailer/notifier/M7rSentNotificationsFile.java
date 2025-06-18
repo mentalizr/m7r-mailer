c1f89dbbcf064606086ee19d72d193cef67810db
Arthur Picht <picht@arthurpicht.de>
2025-06-18 14:28:19 +0200

    renamed file to sent-notifications.timestamp

--- src/main/java/org/mentalizer/mailer/notifier/M7rSentNotificationsFile.java
+++ src/main/java/org/mentalizer/mailer/notifier/M7rSentNotificationsFile.java
@@ -1,13 +1,12 @@
 package org.mentalizer.mailer.notifier;
 
 import org.mentalizr.commons.paths.M7rFile;
-import org.mentalizr.commons.paths.host.hostDir.M7rHostConfigDir;
 import org.mentalizr.commons.paths.host.hostDir.M7rHostRunDir;
 
 public class M7rSentNotificationsFile extends M7rFile {
 
     public M7rSentNotificationsFile() {
-        super(new M7rHostRunDir(), "sent-notifications");
+        super(new M7rHostRunDir(), "sent-notifications.timestamp");
     }
 
 }
